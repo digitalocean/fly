dedf4b9d6a4f828fc8b3c5cebaedff4d51f4b7ce
Demitri Swan <dswan@digitalocean.com>
2017-05-24 10:04:21 -0700

    Using pointer field

--- commands/trigger_job.go
+++ commands/trigger_job.go
@@ -15,7 +15,7 @@ import (
 
 type TriggerJobCommand struct {
 	Job         flaghelpers.JobFlag `short:"j" long:"job" required:"true" value-name:"PIPELINE/JOB" description:"Name of a job to trigger"`
-	BuildNumber uint64              `short:"b" long:"build" required:"false" description:"Build number for job rebuild"`
+	BuildNumber *uint64             `short:"b" long:"build" required:"false" description:"Build number for job rebuild"`
 	Watch       bool                `short:"w" long:"watch" description:"Start watching the build output"`
 }
 
@@ -36,12 +36,12 @@ func (command *TriggerJobCommand) Execute(args []string) error {
 		return err
 	}
 
-	if buildNumber != 0 {
-		build, err = target.Team().Rebuild(pipelineName, jobName, buildNumber)
+	if buildNumber != nil {
+		build, err = target.Team().Rebuild(pipelineName, jobName, *buildNumber)
 		if err != nil {
 			return err
 		}
-		fmt.Printf("started %s%s #%s %s", pipelineName, jobName, build.Name, buildNumber)
+		fmt.Printf("started %s%s #%s %s", pipelineName, jobName, build.Name, *buildNumber)
 	} else {
 		build, err = target.Team().CreateJobBuild(pipelineName, jobName)
 		if err != nil {
